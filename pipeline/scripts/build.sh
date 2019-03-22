45cb0b087d8c989015093a0680a841290a5fc0ba
Jacek Rzeniewicz <jacek@boclips.com>
2019-03-22 15:06:12 +0000

    Drop dependency check

--- pipeline/scripts/build.sh
+++ pipeline/scripts/build.sh
@@ -8,7 +8,7 @@ version=$(cat version/version)
 
 (
 cd source
-./gradlew -Pversion=${version} clean build dependencyCheckAnalyze --rerun-tasks --no-daemon
+./gradlew -Pversion=${version} clean build --rerun-tasks --no-daemon
 )
 
 cp -a source/* dist/
