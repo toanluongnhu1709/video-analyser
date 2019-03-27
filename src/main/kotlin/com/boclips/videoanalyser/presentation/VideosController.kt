416e66b53dc65486c2ad02ea4c450fed36a1deb5
Rebecca Appleyard <rebecca@boclips.com>
2019-03-27 15:56:10 +0000

    Check video index when processing is done
    Signed-off-by: Jacek Rzeniewicz <jacek@boclips.com>

--- src/main/kotlin/com/boclips/videoanalyser/presentation/VideosController.kt
+++ src/main/kotlin/com/boclips/videoanalyser/presentation/VideosController.kt
@@ -1,18 +1,23 @@
 package com.boclips.videoanalyser.presentation
 
+import com.boclips.videoanalyser.infrastructure.videoindexer.VideoIndexer
 import mu.KLogging
 import org.springframework.web.bind.annotation.*
 
 @RestController
-class VideosController {
+class VideosController(val videoIndexer: VideoIndexer) {
 
     companion object : KLogging() {
         const val INDEXING_PROGRESS_PATH_TEMPLATE = "/v1/videos/{videoId}/check_indexing_progress"
     }
 
     @PostMapping(INDEXING_PROGRESS_PATH_TEMPLATE)
-    fun checkIndexingProgress(@PathVariable videoId: String, @RequestParam("id") videoIndexerId: String, @RequestParam state: String) {
-        logger.info { "Checking for indexing progress in video $videoId: VideoIndexer id $videoIndexerId, state $state" }
+    fun checkIndexingProgress(@PathVariable videoId: String) {
+        logger.info { "Checking for indexing progress in video $videoId" }
+
+        val videoIndex = videoIndexer.getVideoIndex(videoId)
+
+        logger.info { "Video index keywords: ${videoIndex.keywords}, video index topics: ${videoIndex.topics}" }
     }
 }
 
