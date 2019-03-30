b97f84c645d4ca47ea5be573f4712d827000137d
Jacek Rzeniewicz <jacek@boclips.com>
2019-03-30 18:05:21 +0000

    Add logging when publishing events

--- src/main/kotlin/com/boclips/videoanalyser/application/PublishAnalysedVideo.kt
+++ src/main/kotlin/com/boclips/videoanalyser/application/PublishAnalysedVideo.kt
@@ -3,6 +3,7 @@ package com.boclips.videoanalyser.application
 import com.boclips.eventtypes.Topics.ANALYSED_VIDEO_IDS_SUBSCRIPTION
 import com.boclips.videoanalyser.config.messaging.Topics
 import com.boclips.videoanalyser.domain.VideoAnalyserService
+import mu.KLogging
 import org.springframework.cloud.stream.annotation.StreamListener
 import org.springframework.messaging.support.MessageBuilder
 
@@ -10,10 +11,14 @@ class PublishAnalysedVideo(
         private val topics: Topics,
         private val videoAnalyserService: VideoAnalyserService
 ) {
+    companion object : KLogging()
+
     @StreamListener(ANALYSED_VIDEO_IDS_SUBSCRIPTION)
     fun execute(videoId: String) {
+        logger.info { "Requesting analysed video $videoId" }
         val video = videoAnalyserService.getVideo(videoId)
 
+        logger.info { "Publishing analysed video $videoId" }
         topics.analysedVideos().send(MessageBuilder.withPayload(video).build())
     }
 }
--- src/main/kotlin/com/boclips/videoanalyser/application/PublishAnalysedVideoId.kt
+++ src/main/kotlin/com/boclips/videoanalyser/application/PublishAnalysedVideoId.kt
@@ -1,11 +1,15 @@
 package com.boclips.videoanalyser.application
 
 import com.boclips.videoanalyser.config.messaging.Topics
+import mu.KLogging
 import org.springframework.messaging.support.MessageBuilder
 
 class PublishAnalysedVideoId(private val topics: Topics) {
 
+    companion object : KLogging()
+
     fun execute(videoId: String) {
+        logger.info { "Video $videoId has been analysed" }
         topics.analysedVideoIds().send(MessageBuilder.withPayload(videoId).build())
     }
 }
