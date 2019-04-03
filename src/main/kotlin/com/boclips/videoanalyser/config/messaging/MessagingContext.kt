8a6b298372d8acff25b3cb1f033f6d55485becb9
Jacek Rzeniewicz <jacek@boclips.com>
2019-04-03 17:15:14 +0100

    Migrate to messaging config extracted to event-types

--- src/main/kotlin/com/boclips/videoanalyser/config/messaging/MessagingContext.kt
+++ src/main/kotlin/com/boclips/videoanalyser/config/messaging/MessagingContext.kt
@@ -1,9 +1,8 @@
 package com.boclips.videoanalyser.config.messaging
 
+import com.boclips.events.config.BoclipsMessagingConfiguration
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.stream.annotation.EnableBinding
-import org.springframework.cloud.stream.config.BindingProperties
-import org.springframework.cloud.stream.config.BindingServiceProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -14,31 +13,5 @@ class MessagingContext(@Value("\${pubsub.topic.suffix}") val topicSuffix: String
 
     @Primary
     @Bean
-    fun bindingServiceProperties(): BindingServiceProperties {
-
-        val bindings = mutableMapOf<String, BindingProperties>()
-
-        TopicDetector.scanTopicChannels(Topics::class).forEach { topic ->
-            bindings[topic.channelName] = topicBindingProperties(topic.topicName)
-        }
-
-        TopicDetector.scanSubscriptionChannels(Subscriptions::class).forEach { topic ->
-            bindings[topic.channelName] = subscriptionBindingProperties(topic.topicName)
-        }
-
-        return BindingServiceProperties().apply {
-            this.bindings = bindings
-        }
-    }
-
-    private fun bindingProperties(topicName: String) = BindingProperties().apply {
-        destination = "$topicName-$topicSuffix"
-    }
-
-    private fun topicBindingProperties(topicName: String) = bindingProperties(topicName)
-
-    private fun subscriptionBindingProperties(topicName: String) =
-            bindingProperties(topicName).apply {
-                group = "video-analyser"
-            }
+    fun bindingServiceProperties() = BoclipsMessagingConfiguration(topicSuffix, "video-analyser").forContext(MessagingContext::class.java)
 }
