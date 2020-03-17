5658e056207348c63fee9a55ee63c27aff8fc041
Andrew Bruce <me@andrewbruce.net>
2020-03-17 16:29:14 +0000

    Fix build.sh for kustomize resource
    [#171666374]

--- pipeline/scripts/build.sh
+++ pipeline/scripts/build.sh
@@ -2,13 +2,14 @@
 
 set -eu
 
-export GRADLE_USER_HOME="$(pwd)/.gradle"
+GRADLE_USER_HOME="$(pwd)/.gradle"
+export GRADLE_USER_HOME
 
-version=$(cat version/version)
+version=$(cat version/tag)
 
 (
 cd source
-./gradlew -Pversion=${version} clean build --rerun-tasks --no-daemon
+./gradlew -Pversion="$version" clean build --rerun-tasks --no-daemon
 )
 
 cp -a source/* dist/
