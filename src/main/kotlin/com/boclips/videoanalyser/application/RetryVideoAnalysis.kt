94cabd98ddc6a399bb308e1ec8fdebcd8d16fa97
Eva Varga <eva@boclips.com>
2020-06-30 13:59:33 +0100

    Make sure video is deleted before reuploading
    It seems that we start the reuploading too soon and the video is not
    yet deleted from the Video Indexer, causing the retrial to stop.
    
    [#173509428]

--- src/main/kotlin/com/boclips/videoanalyser/application/RetryVideoAnalysis.kt
+++ src/main/kotlin/com/boclips/videoanalyser/application/RetryVideoAnalysis.kt
@@ -11,21 +11,40 @@ class RetryVideoAnalysis(
     private val analyseVideo: AnalyseVideo
 ) {
 
-    companion object : KLogging()
+    companion object : KLogging() {
+        private const val MAX_RETRY = 3
+    }
+
 
     @BoclipsEventListener
     fun execute(request: RetryVideoAnalysisRequested) {
         val videoId = request.videoId
-
-        AnalyseVideo.logger.info { "Video $videoId received to retry analysis (language: ${request.language?.toLanguageTag() ?: "detect"})" }
+        logger.info { "Video $videoId received to retry analysis (language: ${request.language?.toLanguageTag() ?: "detect"})" }
 
         try {
             if (videoAnalyserService.isAnalysed(videoId)) {
                 videoAnalyserService.deleteVideo(videoId)
+                waitUntilVideoIsDeleted(videoId)
             }
             analyseVideo.execute(VideoAnalysisRequested(videoId, request.videoUrl, request.language))
         } catch (e: Exception) {
-            AnalyseVideo.logger.warn(e) { "Retry analysis of video $videoId failed and will not be retried." }
+            logger.warn(e) { "Retry analysis of video $videoId failed and will not be retried." }
+        }
+    }
+
+    private fun waitUntilVideoIsDeleted(videoId: String) {
+        var retries = 0
+
+        while (videoAnalyserService.isAnalysed(videoId) && retries < MAX_RETRY) {
+            logger.info { "Waiting for video $videoId to be removed from the index attempt: ${retries + 1}" }
+            retries++
+
+            if (retries < MAX_RETRY) {
+                Thread.sleep(1000)
+            } else {
+                logger.warn { "Video $videoId still not removed from the index, giving up retry attempt" }
+                throw RuntimeException()
+            }
         }
     }
 }
